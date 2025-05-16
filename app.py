ee1160cc2d0f3d3084dd2aa933e5cb68c9a60ac6
Salma Saleh <salmaasalehh2003@gmail.com>
2025-05-16 04:45:45 +0300

    bono

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     # For Docker compatibility
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
